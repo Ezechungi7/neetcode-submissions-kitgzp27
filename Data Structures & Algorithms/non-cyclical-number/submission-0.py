75ee1b7841f4bf47faf394e5ff5019d258e9a094
鍾秉均 <103564296+Ezechungi7@users.noreply.github.com>
2026-05-30 11:06:08 +0800

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        history = [n]
+        while True:
+            if n == 1:
+                return True
+            s = str(n)
+            n = 0
+            for c in s:
+                n += int(c)**2
+            if n in history:
+                return False
+            history.append(n)
+
+            
